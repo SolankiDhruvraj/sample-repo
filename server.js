c633b4aa0d4c2060c76e1fc5de8804d5e64bd0b3
Dhruvraj Solanki <dhruvrajsolanki@Dhruvrajs-MacBook-Air.local>
2026-03-05 21:38:33 +0530

    feat(add route): Add new route

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ import express from "express"
 
 const app = express()
 
+app.get("/", (req, res) => {
+    res.status(200).json({ message: "Hello" })
+})
 app.listen(4001,()=>{
     console.log("Server lsitening")
 })
